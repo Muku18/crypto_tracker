f05085127996732f087f4140f9115b5a54dc308a
Muku18 <88443590+Muku18@users.noreply.github.com>
2021-09-11 16:46:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,116 +1,16 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';   //api call
-// import './App.css';
-// import Coin from './Components/Coin'
-// import Navbar from './Components/Navbar'
-
-// function App() {
-//   const [coins, setcoins] = useState([]);
-//   const [search, setSearch] = useState('');
-
-
-//   useEffect(() => {
-//     axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-//       .then(response => {
-//         setcoins(response.data);
-//         console.log(coins);
-//       })
-//       .catch(error => {
-//         console.log(error.message);
-//       })
-//   }, []);
-
-//   const handleChange = e => {
-//     setSearch(e.target.value);
-//   };
-
-//   const filteredCoins = coins.filter(coin =>
-//     coin.name.toLowerCase().includes(search.toLowerCase())
-//   );
-
-//   console.log(filteredCoins);
-
-//   const listItems = filteredCoins.map((coin) =>
-//     <Coin key={coin.id}
-//       name={coin.name}
-//       symbol={coin.symbol}
-//       image={coin.image}
-//       price={coin.current_price}
-//       volume={coin.total_volume}
-//       market_cap={coin.market_cap}
-//       Chg={coin.price_change_percentage_24h} />
-//   );
-
-
-
-
-//   return (
-//     <div classNameName="App">
-      
-//         {/* <Navbar></Navbar> */}
-      
-
-
-
-
-
-
-
-//       <div classNameName="coins-search">
-//         <h1 classNameName="coins-name"> Search Currency</h1>
-//         <form>
-//           <input type="text" placeholder="search" onChange={handleChange} classNameName="coins-input" />
-//         </form>
-//       </div>
-//       <br />
-//       <div classNameName="Portfolio">
-
-//       </div>
-//       <div classNameName="res">
-//         <table >
-//           <tr >
-//             <td><b>Image</b></td>
-//             <td><b>Name</b></td>
-//             <td><b>Symbol</b></td>
-//             <td><b>Price(INR)</b></td>
-//             <td><b>Volume</b></td>
-//             <td><b>Market cap</b></td>
-//             <td><b>Chg(24H)</b></td>
-
-//           </tr>
-
-
-
-
-//           {listItems}
-//         </table>
-//       </div>
-
-
-
-
-
-
-
-
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';   //api call
 import './App.css';
 import Coin from './Components/Coin'
-//import Portfolio from './Components/Portfolio'
+
 
 import Navbar from './Components/Navbar';
 function App() {
   const [coins, setcoins] = useState([]);
   const [search, setSearch] = useState('');
-  //const [total, setTotal] = useState(0);
+
 
 
   useEffect(() => {
